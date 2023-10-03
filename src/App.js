6037207734442239132e49b379e7809150f7ce8d
JobSeow <job.seow.2017@sis.smu.edu.sg>
2023-10-03 22:20:07 +0800

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {TextField} from "@mui/material";
 
 function App() {
   return (
@@ -9,14 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <TextField id="filled-basic" label="Restaurant name" variant="filled" />
+        </form>
       </header>
     </div>
   );
